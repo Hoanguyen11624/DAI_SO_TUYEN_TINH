11cc280c6c0fb69b35b6a725ac117017e4d07e20
Hoanguyen11624 <155930732+Hoanguyen11624@users.noreply.github.com>
2025-05-22 15:02:40 +0700

    Add files via upload

---
+++ thucthi1.py
@@ -0,0 +1,8 @@
+mylist = [100,200,300]
+a,b,c = mylist
+print(a,b,c)
+
+from trieugoi import tao_danh_sach
+mylist = tao_danh_sach()
+a,b,c = mylist
+print(a,b,c)
---
+++ trieugoi.py
@@ -0,0 +1,3 @@
+def tao_danh_sach():
+    danhsach = [100,200,300]
+    return danhsach
